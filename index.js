e6e41d2e18c5841fa309d06829524acbf631de57
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2016-01-23 12:43:20 +0000

    code refactoring.

--- index.js
+++ index.js
@@ -7,46 +7,55 @@ var app = express();
 // Users Web API
 //////////////////////////////////////////////
 
+// create table Users:
 app.get('/users/create', function (req, res) {
     var action = db.users.create();
     respond(action, res);
 });
 
+// add some initial records:
 app.get('/users/init', function (req, res) {
     var action = db.users.init();
     respond(action, res);
 });
 
+// remove all records from the table:
 app.get('/users/empty', function (req, res) {
     var action = db.users.empty();
     respond(action, res);
 });
 
+// drop the table:
 app.get('/users/drop', function (req, res) {
     var action = db.users.drop();
     respond(action, res);
 });
 
+// add a new user with name:
 app.get('/users/add/:name', function (req, res) {
     var action = db.users.add(req.params.name);
     respond(action, res);
 });
 
+// find a user by id:
 app.get('/users/find/:id', function (req, res) {
     var action = db.users.find(parseInt(req.params.id));
     respond(action, res);
 });
 
+// remove a user by id:
 app.get('/users/remove/:id', function (req, res) {
     var action = db.users.remove(parseInt(req.params.id));
     respond(action, res);
 });
 
+// get all users:
 app.get('/users/all', function (req, res) {
     var action = db.users.all();
     respond(action, res);
 });
 
+// count all users:
 app.get('/users/total', function (req, res) {
     var action = db.users.total();
     respond(action, res);
@@ -56,21 +65,25 @@ app.get('/users/total', function (req, res) {
 // Products Web API
 //////////////////////////////////////////////
 
+// create table Products:
 app.get('/products/create', function (req, res) {
     var action = db.products.create();
     respond(action, res);
 });
 
+// drop the table:
 app.get('/products/drop', function (req, res) {
     var action = db.products.drop();
     respond(action, res);
 });
 
+// remove all products:
 app.get('/products/empty', function (req, res) {
     var action = db.products.empty();
     respond(action, res);
 });
 
+// add a new product with user Id and name:
 app.get('/products/add/:userId/:name', function (req, res) {
     var values = {
         // handle errors here as needed;
@@ -81,21 +94,25 @@ app.get('/products/add/:userId/:name', function (req, res) {
     respond(action, res);
 });
 
+// find a product by id:
 app.get('/products/find/:id', function (req, res) {
     var action = db.products.find(parseInt(req.params.id));
     respond(action, res);
 });
 
+// remove a product by id:
 app.get('/products/remove/:id', function (req, res) {
     var action = db.products.remove(parseInt(req.params.id));
     respond(action, res);
 });
 
+// get all products:
 app.get('/products/all', function (req, res) {
     var action = db.products.all();
     respond(action, res);
 });
 
+// count all products:
 app.get('/products/total', function (req, res) {
     var action = db.products.total();
     respond(action, res);
